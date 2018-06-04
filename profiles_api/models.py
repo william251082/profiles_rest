bc9c7093608449da381666111f336bfa2b99bf06
William del Rosario <williamdelrosario@yahoo.com>
2018-06-04 10:55:04 +0200

    Added profile listing and creating feature

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -42,7 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    object = UserProfileManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
